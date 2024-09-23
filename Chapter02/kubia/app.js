456561ddf75b78336a3c1bc8dce2ab9eaa39644a
Anubhav Thakur <32287141+anubhav1232@users.noreply.github.com>
2024-09-23 12:55:47 -0400

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("Anubhav has hit " + os.hostname() + "\n");
+  response.end("Anubhav has hit the nodejs server" + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
